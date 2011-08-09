6595b087a69392fb28ee31b1cf432a86e55ef131
Casey Link <unnamedrambler@gmail.com>
2011-08-09 11:59:42 -0400

    Spruce up the encoder with a mimetype as well as implement a custom Decoder

--- src/jsonify.py
+++ src/jsonify.py
@@ -14,7 +14,8 @@ class QuoteEncoder(json.JSONEncoder):
     ''' a custom JSON encoder for Quote objects '''
     def default(self, q):
         if isinstance(q, Quote):
-            return { 'id': q.id,
+            return { 'mimetype': Quote.mimetype+'+json', 
+                     'id': q.id,
                      'up': q.up_votes,
                      'down': q.down_votes,
                      'body': q.body, 
@@ -30,5 +31,24 @@ class QuoteEncoder(json.JSONEncoder):
         else:
             return json.JSONEncoder.default(self, q)
  
+class QuoteDecoder(json.JSONDecoder):
+    ''' Custom decoder class. Can throw TypeErrors '''
+    def default(self, json_obj):
+        if json_obj['mimetype'] == Quote.json_mimetype:
+            q = Quote()
+            q.id = int(json_obj['id'])
+            q.up_votes = int(json_obj['up'])
+            q.down_votes = int(json_obj['down'])
+            q.body = json_obj['body']
+            q.tags = [QuoteDecoder.default(self, t) for t in json_obj['tags']]
+            return q
+        elif json_obj['mimetype'] == Tag.json_mimetype:
+            t = Tag()
+            t.id = int(json_obj['id'])
+            t.tag = json_obj['tag']
+            return tag
+        else:
+            return json.JSONDecoder.default(self, json_obj)
+
 def jsonify(data):
     return Response(json.dumps(data, cls=QuoteEncoder), mimetype="application/json")
